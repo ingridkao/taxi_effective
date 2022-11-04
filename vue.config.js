84f8fca4186c81cd4c992328b42aa09e04a0fc04
ingrid <ingrid7613@gmail.com>
2022-11-04 16:33:31 +0800

    Add: add stage env

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const PORT = process.env.VUE_APP_PORT
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
+  publicPath: process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'stage'
     ? '/taxi_effective/'
     : '/',
   devServer: {
